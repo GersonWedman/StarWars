96e4d4ade11a26ce8b1383a54a3e317f50a3de51
GersonWedman <gersonwedman@outlook.com>
2023-02-23 13:51:21 -0300

    Star Wars - S08 - Trello

---
+++ src/components/star-wars/star-wars.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-star-wars',
+  templateUrl: './star-wars.component.html',
+  styleUrls: ['./star-wars.component.css']
+})
+export class StarWarsComponent {
+
+}
