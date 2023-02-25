e7eda3827fccc3f723f71d3b88f97cccc57ea96d
GersonWedman <gersonwedman@outlook.com>
2023-02-25 18:30:48 -0300

    Update 25/02 18:31

--- src/app/personagens-star-wars.service.ts
+++ src/app/personagens-star-wars.service.ts
@@ -1,6 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 import { API_PATH } from '../environments/environments'
+import { PersonagemClass } from './personagem-class';
 
 @Injectable({
   providedIn: 'root'
@@ -9,12 +11,13 @@ import { API_PATH } from '../environments/environments'
 export class PersonagensStarWarsService {
 
   private url = "http://localhost:3000/personagens";
-  //private urlEnv = API_PATH;
+  //private url = API_PATH;
 
   constructor(private _httpClient: HttpClient) { }
 
-  
+  public getPersonagens(): Observable<PersonagemClass[]>{
 
-
-
+    return this._httpClient.get<PersonagemClass[]>(`${API_PATH}`)
+   
+  }
 }
